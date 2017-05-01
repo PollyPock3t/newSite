45a67c2e4ef4431d420018daf5a89895e97888af
Paulline Mallea <Malleapaulline@gmail.com>
2017-04-30 20:42:15 -0400

    second commit: Updates to site
    Updates About page, Nav bar, and footer

---
+++ js/main.js
@@ -0,0 +1,32 @@
+window.onload = function onLoad() {
+	// var bar = new ProgressBar.Line(container, {
+	//   strokeWidth: 4,
+	//   easing: 'easeInOut',
+	//   duration: 1400,
+	//   color: '#FFEA82',
+	//   trailColor: '#eee',
+	//   trailWidth: 1,
+	//   svgStyle: {width: '100%', height: '100%'},
+	//   from: {color: '#FFEA82'},
+	//   to: {color: '#ED6A5A'},
+	//   step: (state, bar) => {
+	//     bar.path.setAttribute('stroke', state.color);
+	//   }
+	// });
+
+	// bar.animate(.8);  // Number from 0.0 to 1.0
+
+	// $(".progress-bar").eq(0); // First bar
+	// $(".progress-bar").eq(1); // Second bar
+	// $(".progress-bar").eq(2); // third bar
+
+	var $bars = $(".progress-bar");
+
+	// $bars.eq(0).animate({width: "80%"}, 2500);
+	// $bars.eq(1).animate({width: "5%"}, 2500);
+	// $bars.eq(1).animate({width: "5%"}, 2500);
+
+};
+
+
+
